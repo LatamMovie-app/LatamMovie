26b6f0b6d9ada332919b63de27309c2074eb8956
Rocciescobar <roccisen.88@gmail.com>
2018-01-19 09:23:16 -0500

    Resolviendo conflictos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /* Vista splash de index 3 segundos */
 $(document).ready(function() {
-    setTimeout(function() { 
-      window.location.href = 'views/home.html';
-    }, 5000);
-  });
+  setTimeout(function() { 
+    window.location.href = 'views/home.html';
+  }, 3000);
+});
